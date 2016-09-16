0885e07e62d5fd2e94cecd229e12dd6641fa07ba
Logiconictvtube <Logiconictvtube@gmail.com>
2016-09-15 22:08:23 -0400

    Update Blackjack_HW.rb

--- Blackjack_HW.rb
+++ Blackjack_HW.rb
@@ -1,1 +1,57 @@
-j, q, k = 10
+def user_hand
+  puts "Please enter your first card."
+  one = gets.chomp.to_i
+
+  puts "Please enter your second card."
+  two = gets.chomp.to_i
+
+  puts "Please enter the dealers card."
+  dealer_card = gets.chomp.to_i
+
+end
+
+puts "Please enter your first card."
+one = gets.chomp.to_i
+
+puts "Please enter your second card."
+two = gets.chomp.to_i
+
+puts "Please enter the dealers card."
+dealer_card = gets.chomp.to_i
+
+cards = {"player" => one + two,
+         "dealer" => dealer_card
+}
+
+loop do
+
+
+  if cards["player"] < 17 && cards["dealer"] > 6
+    puts "You should hit, get another card."
+  elsif cards["player"] < 17 && cards["dealer"] < 6
+    puts "You should hit, get another card."
+  else
+    puts "you should stand where you are."
+  end
+
+  puts "Please enter your first card."
+  one = gets.chomp.to_i
+  if one == 0
+    break
+  end
+
+  puts "Please enter your second card."
+  two = gets.chomp.to_i
+  if two == 0
+    break
+  end
+
+  puts "Please enter the dealers card."
+  dealer_card = gets.chomp.to_i
+  if dealer_card == 0
+    break
+  end
+
+  cards["player"] += one + two
+  cards["dealer"] += dealer_card
+end
