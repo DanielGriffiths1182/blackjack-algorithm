95b38333f691c9035ce203d0c6dbf2c9548bcc2d
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-26 00:02:48 -0500

    brief clean up, ready for revamp.

--- Blackjack_HW.rb
+++ Blackjack_HW.rb
@@ -1,30 +1,26 @@
-def user_hand
-  puts "Please enter your first card."
-  one = gets.chomp.to_i
-
-  puts "Please enter your second card."
-  two = gets.chomp.to_i
-
-  puts "Please enter the dealers card."
-  dealer_card = gets.chomp.to_i
-
-end
-
 puts "Please enter your first card."
 one = gets.chomp.to_i
+if one == 0
+  break
+end
 
 puts "Please enter your second card."
 two = gets.chomp.to_i
+if two == 0
+  break
+end
 
 puts "Please enter the dealers card."
 dealer_card = gets.chomp.to_i
+if dealer_card == 0
+  break
+end
 
 cards = {"player" => one + two,
          "dealer" => dealer_card
 }
 
 loop do
-
 
   if cards["player"] < 17 && cards["dealer"] > 6
     puts "You should hit, get another card."
